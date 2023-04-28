066e8efb84f210a3ac409f502bf3280910f59574
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-04-28 09:53:27 -0400

    passed tests

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,33 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const displayItem = directors.map(d => {
+    return (
+      <div key={d.name}>
+        <h2>{d.name}</h2>
+        <ul>
+          {d.movies.map(movie => {
+            return(
+              <li key={movie}>
+                {movie}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  })
+
+  return (
+    <div>
+      <h1>
+        Directors Page
+      </h1>
+      <div>
+        {displayItem}
+      </div>
+    </div>
+  )
 }
 
 export default Directors;
